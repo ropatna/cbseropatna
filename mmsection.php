a0031a834b31933d2116fbef2bb9cb8de9c10c45
CBSE PATNA COMPUTER UNIT <ropatna.cbse@nic.in>
2022-07-19 18:56:55 +0530

    2011 class xii added

--- mmsection.php
+++ mmsection.php
@@ -32,10 +32,11 @@ if(($_SESSION["cat"] == "mm" || $_SESSION["cat"] == "admin")){
         <label class="text-light bg-dark"> Year : </label>
         <select name="resultyear" id="ryear" class="dropdown">
             <option disabled selected> -Select Year- </option>
-            <option value="2018" <?php if($ryear=="2018") { echo "selected"; } ?>> 2018 </option>
-            <option value="2019" <?php if($ryear=="2019") { echo "selected"; } ?> selected> 2019 </option>
+            <option value="2018" <?php if($ryear=="2018") { echo "selected"; } ?> selected> 2018 </option>
+            <option value="2019" <?php if($ryear=="2019") { echo "selected"; } ?>> 2019 </option>
             <option value="2020" <?php if($ryear=="2020") { echo "selected"; } ?>> 2020 </option>
             <option value="2021" <?php if($ryear=="2021") { echo "selected"; } ?>> 2021 </option>
+            <option value="2011" <?php if($ryear=="2011") { echo "selected"; } ?>> 2011 </option>
         </select>
         <label class="text-light bg-dark">Class : </label>
         <select name="resultclass" id="rclass" class="dropdown">
@@ -66,6 +67,7 @@ if(($_SESSION["cat"] == "mm" || $_SESSION["cat"] == "admin")){
             <option value="2019" <?php if($rmigyear=="2019") { echo "selected"; } ?>> 2019 </option>
             <option value="2020" <?php if($rmigyear=="2020") { echo "selected"; } ?>> 2020 </option>
             <option value="2021" <?php if($rmigyear=="2021") { echo "selected"; } ?>> 2021 </option>
+            <option value="2011" <?php if($rmigyear=="2011") { echo "selected"; } ?>> 2011 </option>
         </select>
         <label class="text-light bg-dark">Class : </label>
         <select name="rmigclass" class="dropdown">
